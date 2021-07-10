e8fdda8c1673eb984b97684fb7abb6eee0d0b686
PremerX007 <bannawat_55120@outlook.com>
2021-07-10 16:10:25 +0700

    Tested TH_Lang

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # 3x3 Matrix Determinant
 print("\n**************************")
 print("* 3x3 Matrix Determinant *")
-print("**************************\n")
+print("**************************")
 
 fx = "y"
 while(fx == "y"):
     while(True):
-        a, b, c = map(int , input("แถว 1 : ").split())
+        a, b, c = map(int , input("\nแถว 1 : ").split())
         d, e, f = map(int , input("แถว 2 : ").split())
         g, h, i = map(int , input("แถว 3 : ").split())
 
