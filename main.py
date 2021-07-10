d9bc361e3b53bd1e4fe81e8fa13e34f09bf6c513
PremerX007 <bannawat_55120@outlook.com>
2021-07-10 16:05:47 +0700

    update

---
+++ main.py
@@ -0,0 +1,18 @@
+# 3x3 Matrix Determinant
+print("\n**************************")
+print("* 3x3 Matrix Determinant *")
+print("**************************\n")
+
+fx = "y"
+while(fx == "y"):
+    while(True):
+        a, b, c = map(int , input("แถว 1 : ").split())
+        d, e, f = map(int , input("แถว 2 : ").split())
+        g, h, i = map(int , input("แถว 3 : ").split())
+
+        det = ((a*e*i)+(b*f*g)+(c*d*h)) - ((g*e*c)+(h*f*a)+(i*d*b))
+        print("\nAnswer : %d"%det)
+        fx = input("[!] Next?? [y/n] .. ")
+        if (fx == "n"):
+            break
+
